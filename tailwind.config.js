fa3c0866f6cbfa0a0767db1435136bb1fc76460c
sajadmsNew <smousavi.ee@gmail.com>
2022-09-06 14:54:04 +0300

    add-tailwindcss-config

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+const labelsClasses = [
+  "indigo",
+  "blue",
+  "red",
+  "purple",
+];
+
+module.exports = {
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    safelist: [
+      ...labelsClasses.map((lbl) => `bg-${lbl}-500`),
+      ...labelsClasses.map((lbl) => `bg-${lbl}-200`),
+      ...labelsClasses.map((lbl) => `text-${lbl}-400`)
+    ],
+  },
+  darkMode: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans"]
+      },
+      gridTemplateColumns: {
+        "1/5": "1fr 5fr"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+}

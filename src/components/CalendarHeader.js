6285e9bc376c752fab00cebdc1988d1993f0cae1
sajadmsNew <smousavi.ee@gmail.com>
2022-09-07 12:00:38 +0300

    feat-add-month-to-header

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -2,29 +2,42 @@ import dayjs from "dayjs";
 import React, { useContext } from "react";
 import logo  from "../assets/logo.png";
 import upwork from '../assets/upwork.png'
-
+import GlobalContext from "../context/GlobalContext";
 export default function CalendarHeader() {
-  
+  const { monthIndex, setMonthIndex } = useContext(GlobalContext);
+  function handlePrevMonth() {
+    setMonthIndex(monthIndex - 1);
+  }
+  function handleNextMonth() {
+    setMonthIndex(monthIndex + 1);
+  }
+  function handleReset() {
+    setMonthIndex(
+      monthIndex === dayjs().month()
+        ? monthIndex + Math.random()
+        : dayjs().month()
+    );
+  }
   return (
     <header className="px-4 py-2 flex items-center">
       <h1 className="mr-10 text-xl text-gray-500 fond-bold">
         Calendar
       </h1>
       <button
-       
+        onClick={handleReset}
         className="border rounded py-2 px-4 mr-5"
       >
         Today
       </button>
       <button 
-     
+      onClick={handlePrevMonth}
       >
         <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
           chevron_left
         </span>
       </button>
       <button 
-    
+      onClick={handleNextMonth}
       >
         <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
           chevron_right
